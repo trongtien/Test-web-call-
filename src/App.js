2203a9f252a5286a5b2048f9bda4733e5362cb57
tienplt <phanletrongtien@gmail.com>
2024-03-21 09:07:53 +0700

    Init app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
+
+<div id="basebs_webcall_plugin" style="display: block; z-index: 2147483647; position: fixed; cursor: pointer; left: unset; right: 30px; bottom: 60px; box-shadow: 0px 9px 28px 8px #0000000D; box-shadow: 0px 6px 16px 0px #00000014; box-shadow: 0px 3px 6px -4px #0000001F;" data-uuid=NRDgIxfQBDvKTUs1heoqGI38f+RiiS00Kr09jqcb8KDxCxC+xzkbIzs53QbzRvBk config-uuid=yTeVczYy79X0FIHglVomsw3vZnl87+IlrkfVaf50wgRZP6EsRoJFjO075XniVEwM>
+<iframe id="collection_modal" src="https://lab.connect247.vn/connect247/plugin/web-call-plugin" frameborder="0" scrolling="no" title="BaseBS web call" style="width: 0px; height: 0px; position: absolute; right: 10px; left: unset; bottom: 75px; max-width: unset; transition: transform 0.3s ease 0s; animation: 0.3s ease 0s 1 normal none running bounceInLeftEffect;"></iframe>
+<img id="call_widget_button" alt="" src="https://lab.connect247.vn/konglab/bcc-user/Resources/WebCallIcon/WebCallIcon.png" style="transition: transform 0.3s ease 0s, box-shadow 0.3s ease 0s; transform: scale(1); filter: none; width: 55px; height: 55px;" class="-inspector-hide-shortcut__">
+</div>
   );
 }
 
